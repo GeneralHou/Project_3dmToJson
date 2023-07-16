b244031ac3f322c46f184cc41b44772fb9fc59b8
GeneralHou <generalhoucn@gmail.com>
2023-07-16 20:51:49 +0800

    [Optimize] solve project to space by searching around

--- N5_Project2Space.py
+++ N5_Project2Space.py
@@ -21,6 +21,8 @@ def Project2Space(surface_name):
     all_sur = rs.filter.surface  # use filter to get all surfaces
     sur_ids = rs.ObjectsByType(all_sur, select=True)  # get ids of surfaces
     surface = sur_ids[0]  # get first one (actually we only have one)
+
+    # begin to project
     projected = {}
     miss_n = 0
     miss_coord_key = []
@@ -30,8 +32,21 @@ def Project2Space(surface_name):
             result = result[0]  # without this line, it will have bug when running the code next line
             projected[i] = [round(result.X,2), round(result.Y,2), round(result.Z,2)]
         else:
-            miss_coord_key.append(i)
-            miss_n += 1
+            # miss_coord_key.append(i)
+            # miss_n += 1
+            factor = 0.01
+            while True:
+                expand_direct = [[0,1,0], [1,0,0], [0,-1,0], [-1,0,0]]
+                for direct in expand_direct:
+                    shift = [sft * factor for sft in direct]
+                    new_coord = [v1+v2 for v1,v2 in zip(coordinates[i], shift)]
+                    result = rs.ProjectPointToSurface(new_coord, surface, (0,0,-1))
+                    if len(result) > 0:
+                        result = result[0]
+                        projected[i] = [round(result.X,2), round(result.Y,2), round(result.Z,2)]
+                        break
+                if len(result) > 0: break
+                factor += 0.01
 
     json_str = json.dumps(projected, indent=4)
     save_dir = './' + 'Surface_' + surface_name + '/' + 'coordinates_space.json'
@@ -50,4 +65,4 @@ def Project2Space(surface_name):
 
 
 if __name__ == '__main__':
-    Project2Space(surface_name='4-000')
+    Project2Space(surface_name='S19_0')
